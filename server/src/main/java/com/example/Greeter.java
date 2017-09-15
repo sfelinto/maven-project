02d101c853bfb1d758a7c1ff8e6e4b5bfa17e430
Sergio Henrique Felinto da Silva <sfelinto@MacBook-Pro-de-Sergio.local>
2017-09-15 09:35:10 -0300

    new commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,7 @@ public class Greeter {
 
   /**
    * @param someone the name of a person
-   * @retun greeting string
+   * @return String string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
